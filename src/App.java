ef5d8efac0a84949568721990f1b35a027d32eec
Gabriel Argolo <gabriel_argolo@outlook.com.br>
2023-03-15 11:13:30 -0300

    Criado Arquivo .properties, e a classe Manipulador que lê o arquivo properties

--- src/App.java
+++ src/App.java
@@ -12,10 +12,16 @@ class App {
 	public static void main(String[] args) throws Exception {
 
 		//acessar endereço da API, fazer a conexao HTTP e buscar os top 250 filmes;
-		String url = "https://imdb-api.com/en/API/MostPopularTVs/k_v4b55g91";
+		String url = Manipulador.getProp().getProperty("prop.key");
 		HttpClient client = HttpClient.newHttpClient();
 		client.newBuilder();
 		URI endereco = URI.create(url);
+
+		String umEstrela = "\uD83D\uDC99";
+		String doisEstrela = "\uD83D\uDC99\uD83D\uDC99";
+		String tresEstrela = "\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99";
+		String quatroEstrela = "\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99";
+		String cincoEstrela = "\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99";
 
 		System.out.println("Iniciando a Conexão...");
 
@@ -24,6 +30,7 @@ class App {
 		String body = response.body();
 		System.out.println(body);
 		System.out.println();
+		
 		//extrair dados, Titulo, Poster e Classificação
 		//PARSEAR OS DADOS
 		JsonParser parser= new JsonParser();
@@ -37,17 +44,23 @@ class App {
 			System.out.println("\u001b[1m\u001b[45mClassificação: " +filme.get(("imDbRating"))+"\u001b[m");
 
 			double stars=0;
-			stars = Double.parseDouble(filme.get("imDbRating"));
+
+			try {
+				stars = Double.parseDouble(filme.get("imDbRating"));	
+			} catch (Exception e ) {
+				System.out.println("Filme sem nota encontrado");
+			}
+
 			if(stars == 0 && stars<=2) {
-				System.out.println("\uD83D\uDC99");
+				System.out.println(umEstrela);
 			}else if(stars>2 && stars<=4) {
-				System.out.println("\uD83D\uDC99\uD83D\uDC99");
+				System.out.println(doisEstrela);
 			}else if(stars>4 && stars <= 6) {
-				System.out.println("\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99");
+				System.out.println(tresEstrela);
 			}else if(stars>6 && stars <=8) {
-				System.out.println("\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99");
+				System.out.println(quatroEstrela);
 			}else if(stars>8) {
-				System.out.println("\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99\uD83D\uDC99");;
+				System.out.println(cincoEstrela);
 			}
 			System.out.println();
 		}
---
+++ src/Manipulador.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Manipulador {
+	public static Properties getProp() throws IOException
+	{
+		Properties props = new Properties();
+		FileInputStream file = new FileInputStream("./key.properties");
+		props.load(file);
+		return props;
+
+	}
+
+//	public static void main(String args[]) throws IOException{
+//		System.out.println("**********Teste usando arquivo de propriedades**********");
+//		Properties prop = getProp();
+//		System.out.println(prop.getProperty("prop.key"));
+//	}
+}
